7a67df909c68a794bc4d651f9f81c6396f08ce33
Riinu <riinuots@gmail.com>
2020-12-15 22:11:19 +0000

    Day 15 - Part II

--- solutions/day15/rambunctious_recitation.R
+++ solutions/day15/rambunctious_recitation.R
@@ -1,26 +1,28 @@
-library(tidyverse)
+# no libraries!!!
+input = scan("solutions/day15/input", sep = ",")
 
-input = scan("solutions/day15/input-test", sep = ",")
+# shorthand for as.character():
 chr = function(x){as.character(x)}
 
+# Parts I and II (modified it for Part II, see previous commit)
 H <- new.env()
 for (location in seq_along(input)) {
-  H[[as.character(input[location])]] = location
+  H[[as.character(input[location])]] = list(last = as.numeric(location))
 }
-last_value = last(input)
+last_value = dplyr::last(input)
 start_time = Sys.time()
-for (turn in (length(input) + 1):2020){
-  #turn = 5
+for (turn in (length(input) + 1):30000000){
+  #turn = 8
   #print(turn)
-  previous_locations = H[[chr(last_value)]]
-  if (length(previous_locations) == 1){
-    new_value = 0
-    
+  #print(last_value)
+  if (is.null(H[[chr(last_value)]]$prev)){
+    new_value = as.numeric(0)
   } else {
-    new_value = last(previous_locations) - nth(previous_locations, -2)
+    new_value = H[[chr(last_value)]]$last - H[[chr(last_value)]]$prev
   }
   #print(new_value)
-  H[[chr(new_value)]] = c(last(H[[chr(new_value)]]), turn)
+  H[[chr(new_value)]]$prev = H[[chr(new_value)]]$last
+  H[[chr(new_value)]]$last = turn
   last_value = new_value
 }
 Sys.time() - start_time
