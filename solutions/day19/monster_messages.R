5a793e42d435f3f16aa6458d0facbf1a0087461e
Riinu <riinuots@gmail.com>
2020-12-19 17:35:34 +0000

    Day 19 Part I

--- solutions/day19/monster_messages.R
+++ solutions/day19/monster_messages.R
@@ -1,7 +1,6 @@
 library(tidyverse)
-library(glue)
 
-input = tibble(input = read_lines("solutions/day19/input-test"))
+input = tibble(input = read_lines("solutions/day19/input"))
 
 split_at = which(input$input == "")
 
@@ -12,41 +11,48 @@ messages = input %>%
 
 rules_orig = input %>% 
   slice(1:(split_at-1)) %>% 
-  separate(input, into = c("id", "rule"), sep = ": ") %>% 
-  mutate(rule = str_remove_all(rule, "\\\"") %>% 
-           str_remove_all(" ")) %>% 
-  #separate_rows(rule, sep = "\\|") %>% 
+  separate(input, into = c("id", "rule"), sep = ": ", convert = TRUE) %>% 
+  mutate(rule = str_remove_all(rule, "\\\"")) %>% 
   arrange(id)
 
-zero  = slice(rules_orig,  1)$rule
-rules = slice(rules_orig, -1)
-loc = 1
+rules = rules_orig %>% 
+  mutate(rule = if_else(str_detect(rule, " "), paste0("( ", rule, " )"), rule))
+
+rules_mod = rules %>%
+  mutate(rule = str_replace_all(rule, " 8 ", " 42 ")) %>%
+  filter(id != 8)
+
 while (TRUE){
-  current_rule = str_sub(zero, loc, loc)
-  while (! str_detect(current_rule, "[:digit:]")){
-    loc = loc + 1
-    current_rule = str_sub(zero, loc, loc)
-  }
-  replacement = filter(rules, id == current_rule)$rule
-  if (str_detect(replacement, "\\|")){
-    replacement = paste0("(", replacement, ")")
-  }
-  zero = str_replace(zero, fixed(current_rule), fixed(replacement))
-  if (! str_detect(zero, "[:digit:]")){
-    return(zero)
-    break
-  }
-  loc = loc + 1
-  #stopifnot(loc < 10)
-  #print(zero)
-  #if (loc %% 10 == 0){print(zero)}
+
+finals = rules_mod %>% 
+  filter(! str_detect(rule, "[:digit:]"))
+
+print(nrow(finals))
+
+for (i in 1:nrow(finals)){
+  #i = 1
+  #if (42 %in% finals$id){browser()}
+  myid = paste0(" ", finals$id[i], " ")
+  myrule = paste0(" ", finals$rule[i], " ")
+  rules_mod = rules_mod %>% 
+    mutate(rule = str_replace_all(rule, myid, myrule))
+}
+rules_mod = rules_mod %>% 
+  #filter(! id %in% finals$id) %>% 
+  mutate(rule = if_else(str_detect(rule, "\\|") | str_detect(rule, "[:digit:]"),
+                        rule,
+                        str_remove_all(rule, "[[:punct:]]| "))) 
+
+if(nrow(finals) == 130){break}
 }
 
-zero = paste0("^", zero, "$")
+
+zero_combine  = rules_mod %>% 
+  filter(id == 0) %>% 
+  pull(rule) %>% 
+  paste0("^", ., "$") %>% str_remove_all(" ")
+
 messages %>% 
-  mutate(is_valid = str_detect(message, zero)) %>% 
+  mutate(is_valid = str_detect(message, zero_combine)) %>% 
   summarise(sum(is_valid))
-
-
-
 
